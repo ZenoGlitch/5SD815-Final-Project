a26d30e012ba96ec7edc9d71b27a24d12a743120
Sebastian <sebastian.norderang.6229@student.uu.se>
2024-01-12 15:58:45 +0100

    suppressing raylib warnings!

--- Source/barrier.cpp
+++ Source/barrier.cpp
@@ -3,12 +3,12 @@
 const void Barrier::Render(Texture2D texture) const noexcept
 {
 	constexpr float texture_offset = 100.0f;
-	DrawTexture(texture, position.x - texture_offset, position.y - texture_offset, WHITE);
+	DrawTextureV(texture, { position.x - texture_offset, position.y - texture_offset }, WHITE);
 
-	constexpr float text_offset_x = 21.0f;
-	constexpr float text_offset_y = 10.0f;
-	constexpr float font_size = 40.0f;
-	DrawText(TextFormat("%i", health), position.x - text_offset_x, position.y + text_offset_y, font_size, RED);
+	constexpr float text_offset_x{ 21.0f };
+	constexpr float text_offset_y { 10.0f };
+	constexpr int font_size { 40 };
+	DrawText(TextFormat("%i", health), static_cast<int>(position.x - text_offset_x), static_cast<int>(position.y + text_offset_y), font_size, RED);
 }
 
 bool Barrier::IsAlive() const noexcept
--- Source/game.h
+++ Source/game.h
@@ -1,4 +1,12 @@
 #pragma once
+
+#include <codeanalysis\warnings.h>
+#pragma warning(push)
+#pragma warning(disable:ALL_CODE_ANALYSIS_WARNINGS)
+#include <raylib.h>
+#include <raymath.h>
+#pragma warning(pop)
+
 #include "raylib.h"
 #include "raymath.h"
 #include "Resources.h"
